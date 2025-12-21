e23b245d24b451fb6e09cfca6e932f5d26039ded
Piotr Ploskonka <pploskonka7@gmail.com>
2025-12-21 18:36:25 +0100

    Update menu and add player input

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def show_menu():
-    pass
+    print("1. Start wyścigu\n2. Statystyki kierowcy\n3. Zapisz grę\n4. Wczytaj grę\n5. Wyjście\n")
+    choice = input("Wybierz opcję: ")
+    return choice
 
 def main():
     print("Welcome to the F1 game!")
